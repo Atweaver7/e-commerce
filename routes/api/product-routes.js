8e3a20a1f99997802fbd67e583abe2d0c6e4cc0c
Aaron Weaver <atweaver7@gmail.com>
2022-03-28 15:36:24 -0700

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,17 +6,67 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll( {
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },{
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  }).then(prodData => res.json(prodData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findOne({
+    where: {
+
+      id: req.params.id
+    },
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    },
+  {
+    model: Tag,
+    attributes: ['tag_name']
+  }]
+  }).then(prodData => res.json(prodData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
 router.post('/', (req, res) => {
+  Product.create(req.body)
+  .then((product) => {
+    if (req.body.tagIds.length){
+      let tagArray = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        }
+      }); return ProductTag.bulkCreate(tagArray);
+    }res.status(200).json(product);
+    }).then((tagId) => {
+      res.status(200).json(tagId)
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err)
+      })
+    })
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -91,6 +141,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(prodData => {
+    if (!prodData) {
+      res.status(400).json({ message: 'No product available with this ID.'})
+      return;
+    }res.json(prodData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
